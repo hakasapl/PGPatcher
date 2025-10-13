b45819117a8c94cedf92687b30536362d4bede1b
hakasapl <hakansaplakog@gmail.com>
2025-10-13 19:23:01 -0400

    pgwarnings mod

--- PGLib/include/ParallaxGenWarnings.hpp
+++ PGLib/include/ParallaxGenWarnings.hpp
@@ -22,19 +22,9 @@ private:
     };
 
     // Trackers for warnings
-    static std::unordered_map<std::wstring, std::unordered_set<std::wstring>>
-        s_mismatchWarnTracker; // matched mod to set of base mods
-    static std::mutex s_mismatchWarnTrackerMutex; /** Mutex for MismatchWarnTracker */
     static std::unordered_map<std::wstring, std::unordered_set<std::pair<std::wstring, std::wstring>, PairHash>>
-        s_mismatchWarnDebugTracker;
-    static std::mutex s_mismatchWarnDebugTrackerMutex; /** Mutex for MismatchWarnDebugTracker */
-
-    static std::unordered_set<std::pair<std::wstring, std::wstring>, PairHash>
-        s_meshWarnTracker; /** Keeps tabs on WARN mesh mismatch messages to avoid duplicates */
-    static std::mutex s_meshWarnTrackerMutex; /** Mutex for MeshWarnTracker */
-    static std::unordered_set<std::pair<std::wstring, std::wstring>, PairHash>
-        s_meshWarnDebugTracker; /** Keeps tabs on DEBUG mesh mismatch messages to avoid duplicates */
-    static std::mutex s_meshWarnDebugTrackerMutex; /** Mutex for MeshWarnDebugTracker */
+        s_mismatchWarnTracker;
+    static std::mutex s_mismatchWarnTrackerMutex; /** Mutex for MismatchWarnDebugTracker */
 
 public:
     static void init();
