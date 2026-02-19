485fcd5eb031895465885ebdf15c81ea8a9b5bc9
hakasapl <hakansaplakog@gmail.com>
2026-02-18 23:20:17 -0500

    fixes2

--- PGLib/include/util/EnumStringHelper.hpp
+++ PGLib/include/util/EnumStringHelper.hpp
@@ -14,16 +14,17 @@ template <typename Enum> struct EnumStringEntry {
 };
 
 template <typename Enum, size_t N>
-constexpr auto enumFromString(
-    std::string_view str, const std::array<EnumStringEntry<Enum>, N>& table, Enum defaultValue) -> Enum
+constexpr auto
+enumFromString(std::string_view str, const std::array<EnumStringEntry<Enum>, N>& table, Enum defaultValue) -> Enum
 {
     auto it = std::ranges::find(table, str, &EnumStringEntry<Enum>::name);
     return (it != table.end()) ? it->value : defaultValue;
 }
 
 template <typename Enum, size_t N>
-constexpr auto stringFromEnum(
-    Enum value, const std::array<EnumStringEntry<Enum>, N>& table, std::string_view defaultValue) -> std::string_view
+constexpr auto stringFromEnum(Enum value,
+                              const std::array<EnumStringEntry<Enum>, N>& table,
+                              std::string_view defaultValue) -> std::string_view
 {
     auto it = std::ranges::find(table, value, &EnumStringEntry<Enum>::value);
     return (it != table.end()) ? it->name : defaultValue;
@@ -41,4 +42,4 @@ auto allEnumStrings(const std::array<EnumStringEntry<Enum>, N>& table) -> std::v
 
     return result;
 }
-};
+}
